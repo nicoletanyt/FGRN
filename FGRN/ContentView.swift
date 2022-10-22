20681f790b92bc368775878b035d0d429cb1d51e
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2022-10-22 10:40:21 +0800

    Initial Commit

---
+++ FGRN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FGRN
+//
+//  Created by NICOLE TAN YITONG stu on 22/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FGRN/FGRNApp.swift
@@ -0,0 +1,17 @@
+//
+//  FGRNApp.swift
+//  FGRN
+//
+//  Created by NICOLE TAN YITONG stu on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FGRNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
