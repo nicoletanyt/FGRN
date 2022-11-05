f5ea62f2d63ca80238bd0b998860183d4e704b3a
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2022-11-05 10:09:14 +0800

    Added questions in the question struct and the dropdown menu for some questions

--- FGRN/EnterInputPage.swift
+++ FGRN/EnterInputPage.swift
@@ -6,8 +6,13 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct EnterInputPage: View {
+//    init() {
+//        UIPickerView.appearance().tintColor = UIColor.red
+//    }
+    
     @State var question: Question
     @State var currentQuestionNum: Int
     @State var currentProgress: CGFloat = 0.0
@@ -20,67 +25,78 @@ struct EnterInputPage: View {
                     .foregroundColor(Color.darkTeal)
                     .font(.system(.largeTitle))
                     .padding(.bottom, 200)
-                if question.options != nil {
+                if question.options != [] {
+                    Picker (selection: $question.input) {
+                        ForEach(question.options, id: \.self) { option in
+                            Text(option)
+                                .tag(option)
+                                .foregroundColor(Color.lightBlue)
+                                .font(.system(size: 25))
+                        }
+                    } label: {
+                        Text("Picker")
+                    }
+                    .pickerStyle(.menu)
+                    
+                } else {
                     TextField("Enter Your Input", text: $question.input)
                         .font(.system(size: 25))
                         .foregroundColor(Color.lightBlue)
                 }
                 Divider()
-                 .frame(height: 4)
-                 .background(Color.lightBlue)
-            }
-            .padding(.horizontal, 50)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    ZStack (alignment: .leading) {
-                        RoundedRectangle(cornerRadius: 15)
-                            .foregroundColor(Color.darkTeal)
-                            .frame(width: 300, height: 30)
-                        RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(Color.white)
-                            .frame(width: 300 * currentProgress, height: 15)
-                            .padding(.horizontal, 10)
-                    }
-                    .padding()
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Image(systemName: "questionmark")
+                    .frame(height: 4)
+                    .background(Color.lightBlue)
+            }         .padding(.horizontal, 50)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        ZStack (alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 15)
+                                .foregroundColor(Color.darkTeal)
+                                .frame(width: 300, height: 30)
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundColor(Color.white)
+                                .frame(width: 300 * currentProgress, height: 15)
+                                .padding(.horizontal, 10)
+                        }
                         .padding()
-                        .background(Color.darkTeal)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(50)
-                }
-                ToolbarItem(placement: .bottomBar) {
-                    HStack {
-                        Button {
-                            //Toggle previous question
-                            if currentQuestionNum > 0 {
-                                arrayOfQuestions[currentQuestionNum] = question //saves the question
-                                currentQuestionNum -= 1
-                                question = arrayOfQuestions[currentQuestionNum]
-                                currentProgress -= 0.3
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Image(systemName: "questionmark")
+                            .padding()
+                            .background(Color.darkTeal)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(50)
+                    }
+                    ToolbarItem(placement: .bottomBar) {
+                        HStack {
+                            Button {
+                                //Toggle previous question
+                                if currentQuestionNum > 0 {
+                                    arrayOfQuestions[currentQuestionNum] = question //saves the question
+                                    currentQuestionNum -= 1
+                                    question = arrayOfQuestions[currentQuestionNum]
+                                    currentProgress -= 1/14
+                                }
+                            } label: {
+                                if currentQuestionNum != 0 {
+                                    toggleButton(next: false)
+                                }
                             }
-                        } label: {
-                            if currentQuestionNum != 0 {
-                                toggleButton(next: false)
+                            Spacer()
+                            Button {
+                                //Toggle next question
+                                if currentQuestionNum < arrayOfQuestions.count - 1{
+                                    arrayOfQuestions[currentQuestionNum] = question //saves the question
+                                    currentQuestionNum += 1
+                                    question = arrayOfQuestions[currentQuestionNum]
+                                    currentProgress += 1/14 //14 is the number of questions in the array
+                                }
+                            } label: {
+                                toggleButton(next: true)
                             }
-                        }
-                        Spacer()
-                        Button {
-                            //Toggle next question
-                            if currentQuestionNum < arrayOfQuestions.count - 1{
-                                arrayOfQuestions[currentQuestionNum] = question //saves the question
-                                currentQuestionNum += 1
-                                question = arrayOfQuestions[currentQuestionNum]
-                                currentProgress += 0.3
-//                                CGFloat(1/arrayOfQuestions.count)
-                            }
-                        } label: {
-                            toggleButton(next: true)
                         }
                     }
                 }
-            }
         }
     }
 }
@@ -99,9 +115,9 @@ struct toggleButton: View {
         .cornerRadius(15)
     }
 }
-
-struct EnterInputPage_Previews: PreviewProvider {
-    static var previews: some View {
-        EnterInputPage(question: arrayOfQuestions[0], currentQuestionNum: 0)
-    }
-}
+//
+//struct EnterInputPage_Previews: PreviewProvider {
+//    static var previews: some View {
+//        EnterInputPage
+//    }
+//}
--- FGRN/Questions.swift
+++ FGRN/Questions.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Question {
     var question: String
     var input: String
-    var options: [String]? = []
+    var options: [String] = []
     var questionNumber: Int
 }
 
@@ -18,5 +18,16 @@ var arrayOfQuestions: [Question] = [
     Question(question: "What is your subject?", input: "", questionNumber: 1),
     Question(question: "Enter the recipient's name.", input: "", questionNumber: 2),
     Question(question: "Enter the recipent's job.", input: "", questionNumber: 3),
-//    Question(question: "Choose your salutation.", input: "", options: ["dear \(arrayOfQuestions[0].input), dear [job], to whom it may concern, greetings [name], good [morning / afternoon / evening / day] [name], hi [name]"], questionNumber: 4)
+    
+    Question(question: "Greeting", input: "", options: ["dear [name]", "dear [job]", "to whom it may concern", "greetings [name]", "good [morning / afternoon / evening / day] [name]", "hi [name]"], questionNumber: 4),
+    Question(question: "What is the purpose of your email?", input: "", questionNumber: 5),
+    Question(question: "Point 1", input: "", questionNumber: 6),
+    Question(question: "Action word", input: "", options: ["inform", "ask", "inquire", "advocate", "propose"], questionNumber: 7),
+    Question(question: "Question 1", input: "", questionNumber: 8),
+    Question(question: "Context?", input: "", questionNumber: 9),
+    Question(question: "Info 1", input: "", questionNumber: 10),
+    Question(question: "Info 2", input: "", questionNumber: 11),
+    Question(question: "Culture", input: "", questionNumber: 12),
+    Question(question: "Senstive issues of the audience", input: "", questionNumber: 13),
+    Question(question: "Closing", input: "", options: ["regards", "best regards", "yours sincerely", "yours faithfully", "thanks", "thank you", "thanks in advance", "cheers"], questionNumber: 13)
 ]
