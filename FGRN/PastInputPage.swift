cba2f0bba67b1264089a4a86a1d99000e1227f97
Ryan Tung <88029861+Ryan-Tung@users.noreply.github.com>
2022-11-14 13:48:46 +0800

    FIXED THE BIG ISSUE THAT EVERYONE HATED (padding)

--- FGRN/PastInputPage.swift
+++ FGRN/PastInputPage.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct PastInputPage: View {
-    @EnvironmentObject var 
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
