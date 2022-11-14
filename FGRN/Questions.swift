f22acad4f42c6d235b107c3431b8ce3bc30ff54a
Guan Yi <lee_guan_yi@s2021.ssts.edu.sg>
2022-11-14 15:41:44 +0800

    added new variable in questions question type

--- FGRN/Questions.swift
+++ FGRN/Questions.swift
@@ -13,29 +13,30 @@ struct Question: Identifiable, Codable, Hashable {
     var question: String
     var input: String
     var options: [String] = []
+    var questionType: String
 }
 
 var greetingFills = ["Morning ", "Name "] //Good [morning / afternoon / evening / day] [name]
 
 var arrayOfQuestions: [Question] = [
     //Questions needed for email info
-    Question(question: "When do you want to send your email?", input: "", options: ["Morning", "Afternoon", "Evening", "Day"]),
-    Question(question: "Enter the recipient's name.", input: ""),
-    Question(question: "Enter the recipent's job.", input: ""),
-    Question(question: "What is your subject?", input: ""),
+    Question(question: "When do you want to send your email?", input: "", options: ["Morning", "Afternoon", "Evening", "Day"], questionType:"Basic Info"),
+    Question(question: "Enter the recipient's name.", input: "", questionType:"Basic Info"),
+    Question(question: "Enter the recipent's job.", input: "", questionType:"Basic Info"),
+    Question(question: "What is your subject?", input: "", questionType:"Basic Info"),
     
-    Question(question: "Greeting", input: "", options: ["dear [name]", "dear [job]", "To whom it may concern", "greetings [name]", "Good [morning / afternoon / evening / day] [name]", "hi [name]"]),
-    Question(question: "Action word", input: "", options: ["inform", "ask", "inquire", "advocate", "propose"]),
+    Question(question: "Greeting", input: "", options: ["dear [name]", "dear [job]", "To whom it may concern", "greetings [name]", "Good [morning / afternoon / evening / day] [name]", "hi [name]"], questionType:"Greeting"),
+    Question(question: "Action word", input: "", options: ["inform", "ask", "inquire", "advocate", "propose"], questionType:"Greeting"),
     
-    Question(question: "What is the purpose of your email?", input: ""),
-    Question(question: "Point", input: ""),
-    Question(question: "Question", input: ""),
-    Question(question: "Context?", input: ""),
-    Question(question: "Info 1", input: ""),
-    Question(question: "Info 2", input: ""),
-    Question(question: "Culture", input: ""),
-    Question(question: "Sensitive issues of the audience", input: ""),
+    Question(question: "What is the purpose of your email?", input: "", questionType:"Content"),
+    Question(question: "Point", input: "", questionType:"Content"),
+    Question(question: "Question", input: "", questionType:"Content"),
+    Question(question: "Context?", input: "", questionType:"Content"),
+    Question(question: "Info 1", input: "", questionType:"Content"),
+    Question(question: "Info 2", input: "", questionType:"Content"),
+    Question(question: "Culture", input: "", questionType:"Content"),
+    Question(question: "Sensitive issues of the audience", input: "", questionType:"Content"),
     
-    Question(question: "Closing", input: "", options: ["regards", "best regards", "yours sincerely", "yours faithfully", "thanks", "thank you", "thanks in advance", "cheers"]),
-    Question(question: "What is your name / title?", input: ""),
+    Question(question: "Closing", input: "", options: ["regards", "best regards", "yours sincerely", "yours faithfully", "thanks", "thank you", "thanks in advance", "cheers"], questionType:"Closing"),
+    Question(question: "What is your name / title?", input: "", questionType:"Closing"),
 ]
