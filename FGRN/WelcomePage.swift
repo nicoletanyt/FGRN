af5d5b6f1a475fade2ec3b782303adf3f89c7e8d
faith <89432125+faithchongrui@users.noreply.github.com>
2022-11-16 11:57:06 +0800

    added icons

--- FGRN/WelcomePage.swift
+++ FGRN/WelcomePage.swift
@@ -33,25 +33,25 @@ struct WelcomePage: View {
                     NavigationLink {
                         EnterInputPage(question: languageToggle ? arrayOfChineseQuestions[0] : arrayOfQuestions[0], questionsArray: languageToggle ? arrayOfChineseQuestions : arrayOfQuestions, currentQuestionNum: 0, language: languageToggle)
                     } label: {
-                        ButtonDisplay(text: languageToggle ? "写电邮" : "Email Generating")
+                        ButtonDisplay(text: languageToggle ? "写电邮" : "Generator", image: "envelope.circle.fill")
                         }
                     
                     NavigationLink {
                         PastInputPage(language: languageToggle)
                     } label: {
-                        ButtonDisplay(text: languageToggle ? "历史" : "History")
+                        ButtonDisplay(text: languageToggle ? "历史" : "History", image: "clock.circle.fill")
                     }
                     
                     NavigationLink {
                         TutorialPage(language: languageToggle)
                     } label: {
-                        ButtonDisplay(text: languageToggle ? "教程" : "Tutorial")
+                        ButtonDisplay(text: languageToggle ? "教程" : "Tutorial", image: "questionmark.circle.fill")
                     }
                     
                     NavigationLink {
                         CreditsPage()
                     } label: {
-                        ButtonDisplay(text: languageToggle ? "信用" : "Credits")
+                        ButtonDisplay(text: languageToggle ? "信用" : "Credits", image: "command.circle.fill")
 
                             .font(.system(size: 30))
                             .foregroundColor(Color.darkTeal)
@@ -71,14 +71,22 @@ struct WelcomePage: View {
 
 struct ButtonDisplay: View {
     var text: String
+    var image: String
 
     var body: some View {
-        Text(text)
-            .font(.system(size: 30))
-            .foregroundColor(Color.white)
-            .frame(width: 300, height: 50)
-            .background(Color.lightBlue)
-            .cornerRadius(10)
+        HStack {
+            Text(text)
+                .padding()
+            Spacer()
+            Image(systemName: image)
+                .padding()
+        }
+        .font(.system(size: 30))
+        .foregroundColor(Color.white)
+        .frame(width: 300, height: 50)
+        .background(Color.lightBlue)
+        .cornerRadius(10)
+        
     }
 }
 
